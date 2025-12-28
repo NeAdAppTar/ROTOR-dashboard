0ad18e5f61b07955d3a12e3e4e91c5af46425bbb
NeAdAppTar <TIvan0931@yandex.ru>
2025-12-28 09:42:57 +0300

    new auth logic

--- checkauth.js
+++ checkauth.js
@@ -38,7 +38,7 @@ onAuthStateChanged(auth, async (user) => {
   if (!user) {
     const redirectUrl = encodeURIComponent(window.location.href);
     window.location.href =
-      `https://auth.rotorprov.ru/?redirect=${redirectUrl}`;
+      `https://dashboard.rotorprov.ru/login/?redirect=${redirectUrl}`;
     return;
   }
 
